fc3c3bd0ff4473952b193561490de6b9c7605a96
DavidCrzBlt <cruzdavid158@gmail.com>
2022-07-28 00:14:03 -0500

    Initial commit
    This is the first part of the web page. Initial stage of this project

---
+++ index.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('/home.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('/productos.html')
+
+@app.route('/promociones')
+def promociones():
+    return render_template('/promociones.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('/blog.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('/contacto.html')
+
+@app.route('/aviso_de_privacidad')
+def privacidad():
+    return render_template('/privacidad.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
