5635a6b4a48ad51e003a63f779ace8dd9bf82512
DavidCrzBlt <cruzdavid158@gmail.com>
2022-08-03 14:05:56 -0500

    Email working

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
+from flask_mail import Mail,Message
+import config
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'mail.rcrmateriales.com.mx'
+app.config['MAIL_PORT'] = 26
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = config.MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = config.MAIL_PASSWORD
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
@@ -18,13 +28,29 @@ def promociones():
 def blog():
     return render_template('/blog.html')
 
-@app.route('/contacto')
+@app.route('/contacto',methods=["GET","POST"])
 def contacto():
-    return render_template('/contacto.html')
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        empresa = request.form["empresa"]
+        telefono = request.form["telefono"]
+        email = request.form["email"]
+        mensaje = request.form["mensaje"]
+        datos_contacto = "Nombre: "+ nombre+ "\nEmpresa: " + empresa + "\nTeléfono: " + telefono + "\nCorreo electrónico: " + email + "\nMensaje: " + mensaje
+        msg = Message(subject="Información de contacto",recipients=['rcrproyectos.admon@gmail.com'],body=datos_contacto,sender=config.MAIL_USERNAME)
+        mail.send(msg)
+        return redirect("/datosEnviados")
+    else:
+        return render_template('/contacto.html')
+
+@app.route("/datosEnviados")
+def datosEnviados():
+    return render_template("datosEnviados.html")
+
 
 @app.route('/aviso_de_privacidad')
 def privacidad():
     return render_template('/privacidad.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
